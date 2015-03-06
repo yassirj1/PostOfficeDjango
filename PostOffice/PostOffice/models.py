7e83de80d58e2f957febfa5d4f7617eba58010d4
yassir <yassirj1@gmail.com>
2015-03-05 18:25:55 -0600

    added some tables

--- PostOffice/PostOffice/models.py
+++ PostOffice/PostOffice/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Shipments(models.Model):
 	tracking_number = models.CharField(max_length=200,primary_key=True)
-	customer_id = models.AutoField(primary_key=True)
+	customer_id = models.ForeignKey(Customer, related_name="customer")
 	time_shipped = models.DateTimeField()
-	reciever_address = models.ForeignKey('Address')
+	reciever_address = models.ForeignKey(Address, related_name="address")
 	receiver_name = models.CharField(max_length=200)
 	post_office_shipped_from = models.IntegerField(max_length=5)
 	date_ship = models.DateField()
@@ -17,5 +17,37 @@ class Shipments(models.Model):
 	package_rate = models.IntegerField(max_length=5)
 	signature_required = models.BooleanField()
 	signature_confirmed = models.BooleanField()
+
+class Address(models.Model):
+	address_id = models.AutoField(max_length=10,primary_key=True)
+	street_line1 = models.CharField(max_length=50)
+	street_line2 = models.CharField(max_length=50)
+	street_line3 = models.CharField(max_length=50)
+	street_line4 = models.CharField(max_length=50)
+	city = models.CharField(max_length=20)
+	state = models.IntegerField(max_length=2)
+	country = models.IntegerField(max_length=4)
+	zipcode = models.IntegerField(max_length=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
