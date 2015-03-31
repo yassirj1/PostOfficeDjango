6a93e357df7c462fa8cd20f051a2fe6a9d5f9dd6
rahim11 <rahimjiwani1@gmail.com>
2015-03-31 17:10:44 -0500

    Added Address and Driver

--- PostOffice/PostOffice/serializers.py
+++ PostOffice/PostOffice/serializers.py
@@ -11,3 +11,19 @@ class ShipmentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Delivery_Routes
         fields = ('delivery_route_id', 'zipcode', 'shipments_carried', 'driver_id', 'time_left', 'time_returned', 'last_location')
+
+class AddressSerializer(serializers.ModelSerializer):
+		
+	
+    class Meta:
+        model = Address
+        fields = ('adress_id','street_line1','street_line2',
+		'street_line3','street_line4','city','state','country','zipcode')
+		
+class DriverSerializer(serializers.ModelSerializer):
+
+	class Meta:
+	model = Driver
+	fields = ('driver_id','driver_ssn','driver_name','driver_phone_number',
+	'drug_test_passed')			
+				
