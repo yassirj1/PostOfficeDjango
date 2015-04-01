099768c75f1dac6587ed76249df44214d9a1c2bf
yassir <yassirj1@gmail.com>
2015-03-31 20:57:58 -0500

    we can now add things to the database for addresses

--- PostOffice/PostOffice/serializers.py
+++ PostOffice/PostOffice/serializers.py
@@ -9,6 +9,17 @@ from PostOffice.models import (
     Incoming_Shipments
 )
 
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('customer_id', 'phone_number', 'first_name', 'last_name', 'customer_email', 'date_joined')
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('adress_id','street_line1','street_line2',
+        'street_line3','street_line4','city','state','country','zipcode')
+
 class ShipmentsSerializer(serializers.ModelSerializer):
     customer_id = CustomerSerializer(many=True)
     reciever_address = AddressSerializer(many=True)
@@ -16,39 +27,23 @@ class ShipmentsSerializer(serializers.ModelSerializer):
         model = Shipments
         fields = ('tracking_number', 'customer_id', 'time_shipped', 'reciever_address', 'receiver_name', 'post_office_shipped_from','date_ship', 'est_arrival', 'last_post_office', 'order_status', 'delivery_status', 'package_type', 'package_weight', 'package_rate', 'signature_required', 'signature_confirmed')
 
- class DeliveryRoutesSerializer(serializers.ModelSerializer):
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = ('driver_id','driver_ssn','driver_name','driver_phone_number',
+        'drug_test_passed')
+
+class DeliveryRoutesSerializer(serializers.ModelSerializer):
     driver_id = DriverSerializer(many=True)
     class Meta:
         model = Delivery_Routes
         fields = ('delivery_route_id', 'zipcode', 'shipments_carried', 'driver_id', 'time_left', 'time_returned', 'last_location')
 
-class AddressSerializer(serializers.ModelSerializer):
-		
-	
-    class Meta:
-        model = Address
-        fields = ('adress_id','street_line1','street_line2',
-		'street_line3','street_line4','city','state','country','zipcode')
-		
-class DriverSerializer(serializers.ModelSerializer):
-
-	class Meta:
-	model = Driver
-	fields = ('driver_id','driver_ssn','driver_name','driver_phone_number',
-	'drug_test_passed')			
-				
 class Incoming_ShipmentsSerializer(serializers.ModelSerializer):
-	"""
-	Serializing all the Incoming_shipments
-	"""
 	class Meta:
 		model = Incoming_Shipments
 		fields = ('route_id', 'post_office_shipping_from', 'post_office_shipping_to', 'route_type')
 
-class CustomerSerializer(serializers.ModelSerializer):
-	"""
-	Serializing all the Customer
-	"""
-	class Meta:
-		model = Customer
-		fields = ('customer_id', 'phone_number', 'first_name', 'last_name', 'customer_email', 'date_joined')
+
+
+
---
+++ PostOffice/PostOffice/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from PostOffice.models import Address
+
+from PostOffice.serializers import AddressSerializer
+
+def index_view(request):
+    """
+    Ensure the user can only see their own profiles.
+    """
+    response = {
+        'addresses': Address.objects.all(),
+    }
+    return render(request, 'index.html', response)
+
+class AddressView(generics.ListCreateAPIView):
+    """
+    Returns a list of all Addresses.
+    """
+    model = Address
+    serializer_class = AddressSerializer
+    def post(self, *args, **kwargs):
+        import pdb; pdb.set_trace()
+
+class AddressInstanceView(generics.RetrieveAPIView):
+    """
+    Returns a single author.
+    Also allows updating and deleting
+    """
+    model = Address
+    serializer_class = AddressSerializer
