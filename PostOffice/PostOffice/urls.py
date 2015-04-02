9c5a243fd4abebd9c431e483fc9d67981cf61044
yassir <yassirj1@gmail.com>
2015-04-02 13:07:46 -0500

    adding stuff

--- PostOffice/PostOffice/urls.py
+++ PostOffice/PostOffice/urls.py
@@ -1,19 +1,34 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from django.conf import settings
 from PostOffice import views
+
+
+
 
 
 urlpatterns = patterns(
     '',
 
-    url(r'^$', views.index_view, name='index_view'),
+    #	url(r'^$', views.index_view, name='index_view'),
     url(r'^addresses/$', views.AddressView.as_view(), name='address-list'),
     url(r'^addresses/(?P<pk>[\d]+)/$', views.AddressInstanceView.as_view(), name = 'address-instance'),
 
     url(r'^shipments/$', views.ShipmentsView.as_view(), name='shipments-list'),
     url(r'^shipments/(?P<pk>[\d]+)/$', views.ShipmentsInstanceView.as_view(), name = 'shipments-instance'),
+
+    
 )
+
+basepatterns = patterns(
+	'',
+	url(r'^api/', include(urlpatterns) ),
+    url(r'^(?P<template_name>\w+)$', views.SimpleStaticView.as_view(), name='PostOffice'),
+    url(r'^$', views.TemplateView.as_view(template_name='index.html')),
+)
+
+
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)	
--- PostOffice/PostOffice/views.py
+++ PostOffice/PostOffice/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
 from rest_framework import generics
-
+from django.views.generic import TemplateView
+from django.conf import settings
 from PostOffice.models import Address, Shipments
 
 from PostOffice.serializers import AddressSerializer, ShipmentsSerializer
+
+class SimpleStaticView(TemplateView):
+    def get_template_names(self):
+        return [self.kwargs.get('template_name') + ".html"]
+        
+    def get(self, request, *args, **kwargs):
+        return super(SimpleStaticView, self).get(request, *args, **kwargs)
 
 def index_view(request):
     """
