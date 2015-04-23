d4c6beeb350da3bc752dbbf06f554dbcce82eb41
yassir <yassirj1@gmail.com>
2015-04-22 19:01:39 -0500

    made the webpage stateless

--- PostOffice/src/js/apiscipts/app.js
+++ PostOffice/src/js/apiscipts/app.js
@@ -9,12 +9,29 @@ angular.module('postOfficeApp')
 .config(function ($stateProvider, $urlRouterProvider) {
 	$stateProvider
 		.state('home', {
-			url:'/',
+			url:'',
 			templateUrl: 'static/views/partial-home.html'
 		})
 		.state('home.tracking', {
-			url: 'tracking',
+			url: '/tracking',
 			templateUrl: 'static/views/tracker.html',
 			controller: 'trackingCtrl'
+		})
+		.state('managedrivers', {
+			url: '/managedrivers',
+			templateUrl: 'static/views/managedrivers.html'
+		})
+		.state('managebranch', {
+			url: '/managebranch',
+			templateUrl: 'static/views/managebranch.html'
+		})
+		.state('manageshipments', {
+			url: '/manageshipments',
+			templateUrl: 'static/views/manageshipments.html'
+		})
+		.state('reports', {
+			url: '/reports',
+			templateUrl: 'static/views/reports.html'
 		});
+		$urlRouterProvider.otherwise("")
 });
