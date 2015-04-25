2cf66eb97a03aac82527dd1444f5c41434c03b8c
Sarah Moore <SarahNinety@hotmail.com>
2015-04-25 17:04:42 -0500

    Added more states for the web pages

--- PostOffice/PostOffice/static/js/script.js
+++ PostOffice/PostOffice/static/js/script.js
@@ -22,14 +22,53 @@ angular.module('postOfficeApp')
 			url: '/managedrivers',
 			templateUrl: 'static/views/managedrivers.html'
 		})
+    .state('managedrivers.createdriver', {
+      url: '/createdriver',
+      templateUrl: 'static/views/createdriver.html'
+    })
+    .state('managedrivers.viewdriver', {
+      url: '/viewdriver',
+      templateUrl: 'static/views/viewdriver.html'
+    })
+    .state('managedrivers.viewroute', {
+      url: '/viewroute',
+      templateUrl: 'static/views/viewroute.html'
+    })
+    .state('managedrivers.editdriver', {
+      url: '/editdriver',
+      templateUrl: 'static/views/editdriver.html'
+    })
 		.state('managebranch', {
 			url: '/managebranch',
 			templateUrl: 'static/views/managebranch.html'
 		})
+    //Shipment states
 		.state('manageshipments', {
 			url: '/manageshipments',
 			templateUrl: 'static/views/manageshipments.html'
 		})
+    .state('manageshipments.createcustomer', {
+      url: '/createcustomer',
+      templateUrl: 'static/views/createcustomer.html'
+    })
+    .state('mangeshipments.viewcustomer', {
+      url: '/viewcustomer',
+      templateUrl: 'static/views/viewcustomer'
+    })
+    .state('mangeshipments.editcustomer', {
+      url: '/editcustomer',
+      templateUrl: 'static/views/editcustomer'
+    })
+    .state('manageshipments.createshipment', {
+      url: '/createshipment',
+      templateUrl: 'static/views/createshipment'
+    })
+    .state('manageshipments.viewshipment', {
+      url: '/viewshipment',
+      templateUrl: 'static/views/createshipment'
+    })
+
+    //Manage Customers States
 		.state('managecustomers', {
 			url:'/managecustomers',
 			templateUrl: 'static/views/managecustomers.html',
