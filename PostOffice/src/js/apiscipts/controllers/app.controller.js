1d49e45b4c69775f019c91e5bef73b544e155802
rahim11 <rahimjiwani1@gmail.com>
2015-04-26 01:29:11 -0500

    table for driver and new data in db

--- PostOffice/src/js/apiscipts/controllers/app.controller.js
+++ PostOffice/src/js/apiscipts/controllers/app.controller.js
@@ -37,3 +37,32 @@ angular.module('postOfficeApp')
 
 
 }]);
+
+angular.module('postOfficeApp')
+.controller('driverTableCtrl', [ '$scope', '$filter', 'ngTableParams', 'poService',  
+	function ($scope, $filter, ngTableParams, poService) {
+		var data = [];
+
+		poService.getDrivers().success( function (response) {
+			data = response;
+		});
+
+
+		$scope.tableParams = new ngTableParams({
+			page: 1,
+			count: 10
+		},{
+			total: data.length,
+			getData: function($defer, params) {
+				var orderedData = params.sorting()?$filter('orderBy')(data, params.orderBy()):data;
+				$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+			}
+		});
+
+		$scope.sendData = function(putForm) {
+			console.log($scope.driver);
+			console.log($scope.putForm);
+		}
+
+
+}]);
