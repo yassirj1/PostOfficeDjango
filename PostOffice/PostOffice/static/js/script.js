fbcae5c524309e450d24160448e4f94167211887
yassir <yassirj1@gmail.com>
2015-04-26 00:33:04 -0500

    added sarah's changes to appropriate files

--- PostOffice/PostOffice/static/js/script.js
+++ PostOffice/PostOffice/static/js/script.js
@@ -9,45 +9,44 @@ angular.module('postOfficeApp', [
 angular.module('postOfficeApp')
 .config(function ($stateProvider, $urlRouterProvider) {
 	$stateProvider
-		.state('home', {
-			url:'',
-			templateUrl: 'static/views/partial-home.html'
-		})
-		.state('home.tracking', {
-			url: '/tracking',
-			templateUrl: 'static/views/tracker.html',
-			controller: 'trackingCtrl'
-		})
-		.state('managedrivers', {
-			url: '/managedrivers',
-			templateUrl: 'static/views/managedrivers.html'
-		})
-    .state('managedrivers.createdriver', {
-      url: '/createdriver',
-      templateUrl: 'static/views/createdriver.html'
-    })
-    .state('managedrivers.viewdriver', {
-      url: '/viewdriver',
-      templateUrl: 'static/views/viewdriver.html'
-    })
-    .state('managedrivers.viewroute', {
-      url: '/viewroute',
-      templateUrl: 'static/views/viewroute.html'
-    })
-    .state('managedrivers.editdriver', {
-      url: '/editdriver',
-      templateUrl: 'static/views/editdriver.html'
-    })
-		.state('managebranch', {
-			url: '/managebranch',
-			templateUrl: 'static/views/managebranch.html'
-		})
-    //Shipment states
-		.state('manageshipments', {
-			url: '/manageshipments',
-			templateUrl: 'static/views/manageshipments.html'
-		})
-    .state('manageshipments.createcustomer', {
+	.state('home', {
+		url:'',
+		templateUrl: 'static/views/partial-home.html'
+	})
+	.state('home.tracking', {
+		url: '/tracking',
+		templateUrl: 'static/views/tracker.html',
+		controller: 'trackingCtrl'
+	})
+	.state('managedrivers', {
+		url: '/managedrivers',
+		templateUrl: 'static/views/managedrivers.html'
+	})
+	.state('managedrivers.createdriver', {
+		url: '/createdriver',
+		templateUrl: 'static/views/createdriver.html'
+	})
+	.state('managedrivers.viewdriver', {
+		url: '/viewdriver',
+		templateUrl: 'static/views/viewdriver.html'
+	})
+	.state('managedrivers.viewroute', {
+		url: '/viewroute',
+		templateUrl: 'static/views/viewroute.html'
+	})
+	.state('managedrivers.editdriver', {
+		url: '/editdriver',
+		templateUrl: 'static/views/editdriver.html'
+	})
+	.state('managebranch', {
+		url: '/managebranch',
+		templateUrl: 'static/views/managebranch.html'
+	})
+	.state('manageshipments', {
+		url: '/manageshipments',
+		templateUrl: 'static/views/manageshipments.html'
+	})
+	.state('manageshipments.createcustomer', {
       url: '/createcustomer',
       templateUrl: 'static/views/createcustomer.html'
     })
@@ -67,18 +66,16 @@ angular.module('postOfficeApp')
       url: '/viewshipment',
       templateUrl: 'static/views/createshipment'
     })
-
-    //Manage Customers States
-		.state('managecustomers', {
-			url:'/managecustomers',
-			templateUrl: 'static/views/managecustomers.html',
-			controller: 'customerTableCtrl'
-		})
-		.state('reports', {
-			url: '/reports',
-			templateUrl: 'static/views/reports.html'
-		});
-		$urlRouterProvider.otherwise("")
+	.state('managecustomers', {
+		url:'/managecustomers',
+		templateUrl: 'static/views/managecustomers.html',
+		controller: 'customerTableCtrl'
+	})
+	.state('reports', {
+		url: '/reports',
+		templateUrl: 'static/views/reports.html'
+	});
+	$urlRouterProvider.otherwise("")
 });
 'use strict';
 
--- PostOffice/src/js/apiscipts/app.js
+++ PostOffice/src/js/apiscipts/app.js
@@ -9,35 +9,71 @@ angular.module('postOfficeApp', [
 angular.module('postOfficeApp')
 .config(function ($stateProvider, $urlRouterProvider) {
 	$stateProvider
-		.state('home', {
-			url:'',
-			templateUrl: 'static/views/partial-home.html'
-		})
-		.state('home.tracking', {
-			url: '/tracking',
-			templateUrl: 'static/views/tracker.html',
-			controller: 'trackingCtrl'
-		})
-		.state('managedrivers', {
-			url: '/managedrivers',
-			templateUrl: 'static/views/managedrivers.html'
-		})
-		.state('managebranch', {
-			url: '/managebranch',
-			templateUrl: 'static/views/managebranch.html'
-		})
-		.state('manageshipments', {
-			url: '/manageshipments',
-			templateUrl: 'static/views/manageshipments.html'
-		})
-		.state('managecustomers', {
-			url:'/managecustomers',
-			templateUrl: 'static/views/managecustomers.html',
-			controller: 'customerTableCtrl'
-		})
-		.state('reports', {
-			url: '/reports',
-			templateUrl: 'static/views/reports.html'
-		});
-		$urlRouterProvider.otherwise("")
+	.state('home', {
+		url:'',
+		templateUrl: 'static/views/partial-home.html'
+	})
+	.state('home.tracking', {
+		url: '/tracking',
+		templateUrl: 'static/views/tracker.html',
+		controller: 'trackingCtrl'
+	})
+	.state('managedrivers', {
+		url: '/managedrivers',
+		templateUrl: 'static/views/managedrivers.html'
+	})
+	.state('managedrivers.createdriver', {
+		url: '/createdriver',
+		templateUrl: 'static/views/createdriver.html'
+	})
+	.state('managedrivers.viewdriver', {
+		url: '/viewdriver',
+		templateUrl: 'static/views/viewdriver.html'
+	})
+	.state('managedrivers.viewroute', {
+		url: '/viewroute',
+		templateUrl: 'static/views/viewroute.html'
+	})
+	.state('managedrivers.editdriver', {
+		url: '/editdriver',
+		templateUrl: 'static/views/editdriver.html'
+	})
+	.state('managebranch', {
+		url: '/managebranch',
+		templateUrl: 'static/views/managebranch.html'
+	})
+	.state('manageshipments', {
+		url: '/manageshipments',
+		templateUrl: 'static/views/manageshipments.html'
+	})
+	.state('manageshipments.createcustomer', {
+      url: '/createcustomer',
+      templateUrl: 'static/views/createcustomer.html'
+    })
+    .state('mangeshipments.viewcustomer', {
+      url: '/viewcustomer',
+      templateUrl: 'static/views/viewcustomer'
+    })
+    .state('mangeshipments.editcustomer', {
+      url: '/editcustomer',
+      templateUrl: 'static/views/editcustomer'
+    })
+    .state('manageshipments.createshipment', {
+      url: '/createshipment',
+      templateUrl: 'static/views/createshipment'
+    })
+    .state('manageshipments.viewshipment', {
+      url: '/viewshipment',
+      templateUrl: 'static/views/createshipment'
+    })
+	.state('managecustomers', {
+		url:'/managecustomers',
+		templateUrl: 'static/views/managecustomers.html',
+		controller: 'customerTableCtrl'
+	})
+	.state('reports', {
+		url: '/reports',
+		templateUrl: 'static/views/reports.html'
+	});
+	$urlRouterProvider.otherwise("")
 });
